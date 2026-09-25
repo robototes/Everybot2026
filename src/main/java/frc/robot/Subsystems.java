9e7a02ec117bc299671d4e7d7e18f4c7e1a5262d
cwkpete7383 <peterichi55@gmail.com>
2026-09-24 20:09:24 -0700

    spotless applied

--- src/main/java/frc/robot/Subsystems.java
+++ src/main/java/frc/robot/Subsystems.java
@@ -1,8 +1,8 @@
 package frc.robot;
 
 import static frc.robot.Subsystems.SubsystemConstants.DRIVEBASE_ENABLED;
-import static frc.robot.Subsystems.SubsystemConstants.INTAKE_ENABLED;
 import static frc.robot.Subsystems.SubsystemConstants.FLYWHEELS_ENABLED;
+import static frc.robot.Subsystems.SubsystemConstants.INTAKE_ENABLED;
 
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import frc.robot.generated.AlphaTunerConstants;
